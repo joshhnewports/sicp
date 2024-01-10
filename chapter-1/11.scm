773a8596605c331352ad339df9eded5fa1aa8f49
joshhnewports <joshhnewports@gmail.com>
2024-01-10 11:28:29 -0800

    Finish chapter-1/11.scm

--- chapter-1/11.scm
+++ chapter-1/11.scm
@@ -1,8 +1,18 @@
-(define (f n)
+(define (f-tree n)
   (if (< n 3)
       n
-      (else (+ (f (- n 1))
-	       (* 2 (f (- n 2)))
-	       (* 3 (f (- n 3)))))))
+      (+ (f-tree (- n 1))
+	 (* 2 (f-tree (- n 2)))
+	 (* 3 (f-tree (- n 3))))))
 
-
+(define (f-iter a b c m n)
+  (cond ((< n 3) n)
+	((= m 0) c)
+	(else (f-iter b
+		      c
+		      (+ c (* 2 b) (* 3 a))
+		      (- m 1)
+		      n))))
+  
+(define (f n)
+  (f-iter 0 1 2 (- n 2) n))
