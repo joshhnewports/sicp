88ba340c296dca561799acb8ba592b3557924915
joshhnewports <joshhnewports@gmail.com>
2024-01-23 05:05:32 -0800

    Add 27.scm

---
+++ chapter-1/27.scm
@@ -0,0 +1,37 @@
+(define (carmichael? n)
+  (define (fermat-test n a)
+    (if (expmod a n n) a)
+	
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	   
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+	((even? n) (square (fast-expt b (/ n 2))))
+	(else (* b (fast-expt b (- n 1))))))
