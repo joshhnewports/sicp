f17d4258f07b1f1ced430be09f5d4b06954693ed
joshhnewports <joshhnewports@gmail.com>
2024-01-24 08:10:16 -0800

    Add 28.scm

---
+++ chapter-1/28.scm
@@ -0,0 +1,23 @@
+(define (nontrivial-sqrt? m n)
+  (and (not (or (= m 1)
+		(= m (- n 1))))
+       (= (remainder (square m) n) 1)))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((miller-rabin-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+(define (miller-rabin-test n)
+  (define (try-it n a)
+    (= (expmod a (- n 1) n) (remainder 1 n)))
+  (try-it n (+ 1 (random (- n 1)))))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((nontrivial-sqrt? base m) 0)
+	((even? exp) (remainder (square (expmod base (/ exp 2) m))
+				m))
+	(else (remainder (* base (expmod base (- exp 1) m))
+			 m))))
+
