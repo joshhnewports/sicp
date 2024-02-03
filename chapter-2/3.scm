f795974804776d0a2504828963fa667a8215ab9e
joshhnewports <joshhnewports@gmail.com>
2024-02-03 09:16:38 -0800

    Fix github syntax highlighting

--- chapter-2/3.scm
+++ chapter-2/3.scm
@@ -8,29 +8,28 @@
   (* (length (side r 1))
      (length (side r 2))))
 
-(define (length s) ;assume this was written for segments
+(define (length s)
   (distance (start-segment s) (end-segment s)))
 
-(define (distance p q) ;assume this was written for points
+(define (distance p q)
   (sqrt (+ (square (- (x-point p) (x-point q)))
 	   (square (- (y-point p) (y-point q))))))
 
 ;abstraction barrier
 
-(define (side r n) ;returns n-th side of rectangle r
+(define (side r n) 
   (cond ((= n 1) (car (car r)))
 	((= n 2) (cdr (car r)))
 	((= n 3) (car (cdr r)))
 	((= n 4) (cdr (cdr r)))))
 
-(define (make-rectangle p q r s) ;takes four points and returns a pair of pairs of connected segments
+(define (make-rectangle p q r s)
   (cons (cons (make-segment p q)
 	      (make-segment q r))
 	(cons (make-segment r s)
 	      (make-segment s p))))
 
-;alternate representation. holds only points (smaller than segments) but constructs segments with each call to side
-(define (side r n) ;returns the n-th side of rectangle r
+(define (side r n)
   (let ((p (car (car r)))
 	(q (cdr (car r)))
 	(r (car (cdr r)))
@@ -40,6 +39,6 @@
 	  ((= n 3) (make-segment r s))
 	  ((= n 4) (make-segment s p)))))
 
-(define (make-rectangle p q r s) ;takes four points and returns a pair of pairs of points
+(define (make-rectangle p q r s)
   (cons (cons p q)
 	(cons r s)))
