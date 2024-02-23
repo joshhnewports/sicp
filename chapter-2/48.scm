f2b70a6817e9ef5e3be5764e58180992f0c18a08
joshhnewports <joshhnewports@gmail.com>
2024-02-22 19:39:12 -0800

    Add 48.scm

---
+++ chapter-2/48.scm
@@ -0,0 +1,8 @@
+(define (make-segment start-vector end-vector)
+  (cons start-vector end-vector))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
