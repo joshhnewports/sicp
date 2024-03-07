b853f95d9c886e7314271549701b87d01b510898
joshhnewports <joshhnewports@gmail.com>
2024-03-06 23:40:47 -0800

    Finish 60.scm

--- chapter-2/60.scm
+++ chapter-2/60.scm
@@ -4,6 +4,15 @@
 	(else (element-of-set? x (cdr set)))))
 
 (define (adjoin-set x set)
+  (cons x set))
+
+(define (intersection-set s t)
+  (cond ((or (null? s) (null? t)) '())
+	((element-of-set? (car s) t)
+	 (cons (car s) (intersection-set (cdr s) t)))
+	(else (intersection-set (cdr s) t))))
+
+(define (union-set s t)
   
 
 (define (print-set set)
