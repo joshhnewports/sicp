fdb342ea551ea95b14b9dcc6aeedebb060ef5801
joshhnewports <joshhnewports@gmail.com>
2024-03-09 23:28:28 -0800

    Start 63.scm

---
+++ chapter-2/63.scm
@@ -0,0 +1,2 @@
+;;Procedure tree->list-1 generates a recursive process and tree->list-2 generates an iterative process, firstly.
+;;We can also describe tree->list-1 as folding right and tree->list-2 as folding left.
