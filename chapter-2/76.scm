654b5d13ddefe4f95710bd3d57c74c17f01c236f
joshhnewports <joshhnewports@gmail.com>
2024-04-07 06:39:57 -0700

    Add 76.scm

---
+++ chapter-2/76.scm
@@ -0,0 +1,4 @@
+;;For types, message-passing. No other type or table or procedure needs to be changed.
+;;For operations, data-directed. Assuming the types exist, we can add operations as procedures
+;;in packages and put them in a table.
+;;This seems like an opinion-based question. Data-directed might be good for both.
