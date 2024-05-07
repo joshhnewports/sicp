5a5343b3eb880258d1872a6b789261846db3626d
joshhnewports <joshhnewports@gmail.com>
2024-05-07 05:18:44 -0700

    Add 31.scm

---
+++ chapter-3/31.scm
@@ -0,0 +1,2 @@
+;;The difference is that for the actual definition, the actions are put into the agenda on construction.
+;;For the other definition, no actions are added into the agenda on construction. 
