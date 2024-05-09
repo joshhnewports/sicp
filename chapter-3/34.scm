2d32d615ab363395939c6501fd5287d422b2338b
joshhnewports <joshhnewports@gmail.com>
2024-05-08 22:36:07 -0700

    Add 34.scm

---
+++ chapter-3/34.scm
@@ -0,0 +1,4 @@
+;;If b is given a value then a must be the square root of b, and this must be true of both inputs m1 m2.
+;;With the multiplier, suppose a = 0 and b is set to 4. We wish for a to be set to 2.
+;;Note that b would not be set at 0 because b's informant is not this squarer constraint, but is some other
+;;constraint that set it to 4. Possibly by the user.
