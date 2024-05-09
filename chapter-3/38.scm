1dd5a12a78b0866dcb4c85892207f49d8b6f8d8c
joshhnewports <joshhnewports@gmail.com>
2024-05-09 07:47:29 -0700

    Add 38.scm

---
+++ chapter-3/38.scm
@@ -0,0 +1,10 @@
+100 -> 110 -> 90 -> 45  ;peter paul mary
+100 -> 110 -> 55 -> 35  ;peter mary paul
+100 -> 80 -> 90 -> 45   ;paul peter mary
+100 -> 80 -> 40 -> 50   ;paul mary peter
+100 -> 50 -> 60 -> 40   ;mary peter paul
+100 -> 50 -> 30 -> 40   ;mary paul peter
+
+Example for (b).
+Peter could deposit 10 and within this time Paul and Mary complete their transactions causing the balance to go from
+100 to 40, and Peter's transaction leaves the account with 110. 
