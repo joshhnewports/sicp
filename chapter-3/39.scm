a9e04020d1b9820f86334760074d90ed8ad8e949
joshhnewports <joshhnewports@gmail.com>
2024-05-10 01:44:03 -0700

    Add 39.scm

---
+++ chapter-3/39.scm
@@ -0,0 +1,5 @@
+P_2 sets x to 11 then P_1 sets x to 121
+P_1 accesses x, P_2 sets x to 11, then P_1 sets x to 100
+P_1 sets x to 100 then P_2 sets x to 101
+
+Others say 11 is possible. I just don't see it.
