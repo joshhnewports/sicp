7c3e67d73009b35b11c00bfacd9830e855807f53
joshhnewports <joshhnewports@gmail.com>
2024-05-10 04:39:45 -0700

    Add 42.scm

---
+++ chapter-3/42.scm
@@ -0,0 +1,3 @@
+Originally, a call to withdraw would start a call to (protected withdraw). Here, a call to withdraw references
+a local procedure that is created on make-account. Hence (protected withdraw) always exists. I couldn't tell you
+anything more.
