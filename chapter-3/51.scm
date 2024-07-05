3161b464736455bd871e5d33e5aa2e846bd4351b
joshhnewports <joshhnewports@gmail.com>
2024-07-05 16:12:29 -0700

    Finish redoing 51.scm

--- chapter-3/51.scm
+++ chapter-3/51.scm
@@ -5,67 +5,35 @@
 (cons-stream 0 (stream-map show (cons-stream 1 (stream-enumerate-interval 2 10)))) ;=> 0
 
 ;;evaluate (stream-ref x 5)
-(stream-ref (cons-stream 0 (stream-map show (cons-stream 1 (stream-enumerate-interval 2 10)))) 5)
-(stream-ref (stream-cdr (cons-stream 0 (stream-map show (cons-stream 1 (stream-enumerate-interval 2 10))))) 4)
-(stream-ref (
-
-;;---------------------- OLD VERSION
-
-;;on definition of x
-(stream-map show (cons 0 (delay (stream-enumerate-interval 1 10))))
-(cons (show 0) (delay (stream-map show (force (cdr (cons 0 (delay (stream-enumerate-interval 1 10)))))))) ;=> 0
-
-;;evaluate (stream-ref x 5)
 (stream-ref x 5)
-(stream-ref (cons (show 0) (delay (stream-map show (force (cdr (cons 0 (delay (stream-enumerate-interval 1 10)))))))) 5)
-
-(stream-ref (force (cdr (cons (show 0) (delay (stream-map show (force (cdr (cons 0 (delay (stream-enumerate-interval 1 10)))))))))) 4)
-(stream-ref (force (delay (stream-map show (force (cdr (cons 0 (delay (stream-enumerate-interval 1 10)))))))) 4)
-(stream-ref (stream-map show (force (cdr (cons 0 (delay (stream-enumerate-interval 1 10)))))) 4)
-(stream-ref (stream-map show (force (delay (stream-enumerate-interval 1 10)))) 4)
-(stream-ref (stream-map show (stream-enumerate-interval 1 10)) 4)
-(stream-ref (stream-map show (cons 1 (delay (stream-enumerate-interval 2 10)))) 4)
-(stream-ref (cons (show 1) (delay (stream-map show (force (cdr (cons 1 (delay (stream-enumerate-interval 2 10)))))))) 4) ;=> 1
-
-(stream-ref (force (cdr (cons (show 1) (delay (stream-map show (force (cdr (cons 1 (delay (stream-enumerate-interval 2 10)))))))))) 3)
-(stream-ref (force (delay (stream-map show (force (cdr (cons 1 (delay (stream-enumerate-interval 2 10)))))))) 3)
-(stream-ref (stream-map show (force (cdr (cons 1 (delay (stream-enumerate-interval 2 10)))))) 3)
-(stream-ref (stream-map show (force (delay (stream-enumerate-interval 2 10)))) 3)
-(stream-ref (stream-map show (stream-enumerate-interval 2 10)) 3)
-(stream-ref (stream-map show (cons 2 (delay (stream-enumerate-interval 3 10)))) 3)
-(stream-ref (cons (show 2) (delay (stream-map show (force (cdr (cons 2 (delay (stream-enumerate-interval 3 10)))))))) 3) ;=> 2
-
-(stream-ref (force (cdr (cons (show 2) (delay (stream-map show (force (cdr (cons 2 (delay (stream-enumerate-interval 3 10)))))))))) 2)
-(stream-ref (force (delay (stream-map show (force (cdr (cons 2 (delay (stream-enumerate-interval 3 10)))))))) 2)
-(stream-ref (stream-map show (force (cdr (cons 2 (delay (stream-enumerate-interval 3 10)))))) 2)
-(stream-ref (stream-map show (force (delay (stream-enumerate-interval 3 10)))) 2)
-(stream-ref (stream-map show (stream-enumerate-interval 3 10)) 2)
-(stream-ref (stream-map show (cons 3 (delay (stream-enumerate-interval 4 10)))) 2)
-(stream-ref (cons (show 3) (delay (stream-map show (force (cdr (cons 3 (delay (stream-enumerate-interval 4 10)))))))) 2) ;=> 3
-
-(stream-ref (force (cdr (cons (show 3) (delay (stream-map show (force (cdr (cons 3 (delay (stream-enumerate-interval 4 10)))))))))) 1)
-(stream-ref (force (delay (stream-map show (force (cdr (cons 3 (delay (stream-enumerate-interval 4 10)))))))) 1)
-(stream-ref (stream-map show (force (cdr (cons 3 (delay (stream-enumerate-interval 4 10)))))) 1)
-(stream-ref (stream-map show (force (delay (stream-enumerate-interval 4 10)))) 1)
-(stream-ref (stream-map show (stream-enumerate-interval 4 10)) 1)
-(stream-ref (stream-map show (cons 4 (delay (stream-enumerate-interval 5 10)))) 1)
-(stream-ref (cons (show 4) (delay (stream-map show (force (cdr (cons 4 (delay (stream-enumerate-interval 5 10)))))))) 1) ;=> 4
-
-(stream-ref (force (cdr (cons (show 4) (delay (stream-map show (force (cdr (cons 4 (delay (stream-enumerate-interval 5 10)))))))))) 0)
-(stream-ref (force (delay (stream-map show (force (cdr (cons 4 (delay (stream-enumerate-interval 5 10)))))))) 0)
-(stream-ref (stream-map show (force (cdr (cons 4 (delay (stream-enumerate-interval 5 10)))))) 0)
-(stream-ref (stream-map show (force (delay (stream-enumerate-interval 5 10)))) 0)
-(stream-ref (stream-map show (stream-enumerate-interval 5 10)) 0)
-(stream-ref (stream-map show (cons 5 (delay (stream-enumerate-interval 6 10)))) 0)
-(stream-ref (cons (show 5) (delay (stream-map show (force (cdr (cons 5 (delay (stream-enumerate-interval 6 10)))))))) 0) ;=> 5
-
-(stream-car (cons 5 (delay (stream-map show (force (cdr (cons 5 (delay (stream-enumerate-interval 6 10)))))))))
+(stream-ref (cons-stream 0 (stream-map show (cons-stream 1 (stream-enumerate-interval 2 10)))) 5)
+(stream-ref (cons-stream 1 (stream-map show (cons-stream 2 (stream-enumerate-interval 3 10)))) 4) ;=> 1
+(stream-ref (cons-stream 2 (stream-map show (cons-stream 3 (stream-enumerate-interval 4 10)))) 3) ;=> 2
+(stream-ref (cons-stream 3 (stream-map show (cons-stream 4 (stream-enumerate-interval 5 10)))) 2) ;=> 3
+(stream-ref (cons-stream 4 (stream-map show (cons-stream 5 (stream-enumerate-interval 6 10)))) 1) ;=> 4
+(stream-ref (cons-stream 5 (stream-map show (cons-stream 6 (stream-enumerate-interval 7 10)))) 0) ;=> 5
 5
 
-;;Hand-made trace of (stream-ref x 5). Possibly wrong.
-;;I have substituted stream-cdr for (force (cdr ...)) and decided to keep (show x) instead of evaluating it to x as well as
-;;showing the number show prints as a comment beside its line.
+;;evaluate (stream-ref x 7)
+(stream-ref x 7)
+(stream-ref (cons-stream 0 (stream-map show (cons-stream 1 (stream-enumerate-interval 2 10)))) 7)
+(stream-ref (cons-stream 1 (stream-map show (cons-stream 2 (stream-enumerate-interval 3 10)))) 6)
+(stream-ref (cons-stream 2 (stream-map show (cons-stream 3 (stream-enumerate-interval 4 10)))) 5)
+(stream-ref (cons-stream 3 (stream-map show (cons-stream 4 (stream-enumerate-interval 5 10)))) 4)
+(stream-ref (cons-stream 4 (stream-map show (cons-stream 5 (stream-enumerate-interval 6 10)))) 3)
+(stream-ref (cons-stream 5 (stream-map show (cons-stream 6 (stream-enumerate-interval 7 10)))) 2)
+(stream-ref (cons-stream 6 (stream-map show (cons-stream 7 (stream-enumerate-interval 8 10)))) 1) ;=> 6
+(stream-ref (cons-stream 7 (stream-map show (cons-stream 8 (stream-enumerate-interval 9 10)))) 0) ;=> 7
+7
 
-;;Now for (stream-ref x 7). We do the very same, except that (show x) is memoized for numbers 1 through 5
-;;and their values are simply returned. We do not compute show, as in (proc), in the definition of memo-proc. Therefore we do not print
-;;each number by means of (display-line x). Since 6 and 7 are unaffected by this, they are displayed.
+;;As to why when running the individual calls of stream-ref we do not change the state of x is because
+;;the stream we have written above is not x but some textual way of representing the stream representing x.
+;;Only on the calls (stream-ref x 5) and (stream-ref x 7) does the state of x change.
+
+;;Suppose we define x then evaluate (stream-ref x 7). The numbers printed are 1 through 7.
+;;Define x once more and evaluate (stream-ref x 5). Then evaluate (stream-ref x 7). The numbers printed are
+;;6 and 7. This is because the display-line expression of show has been memoized out of the procedure and only
+;;the returned value of (show x), that being x, is the value of each call of show to the first 5 numbers
+;;of the stream.
+
+;;We have, in this version, not written cons, delay, force, or stream-cdr, and simply just evaluated up front.
