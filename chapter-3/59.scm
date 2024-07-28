b0186c8305326a5d22e93fa698d97d5a1ed739cf
joshhnewports <joshhnewports@gmail.com>
2024-07-28 02:40:44 -0700

    Finish 59.scm

--- chapter-3/59.scm
+++ chapter-3/59.scm
@@ -1,4 +1,5 @@
 (define (integrate-series coefficients)
   (stream-map / coefficients integers))
 
-(define cosine-series (cons-stream 1 
+(define cosine-series (cons-stream 1 (stream-map - (integrate-series sine-series))))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
