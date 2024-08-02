81618969a622dc77e8f3f70d788986db3f5878d2
joshhnewports <joshhnewports@gmail.com>
2024-08-02 16:00:00 -0700

    Add 68.scm

---
+++ chapter-3/68.scm
@@ -0,0 +1,2 @@
+;;The interleaving of the stream-map and pairs continues forever because there is no delay.
+;;With the cons-stream in the original definition, there is a delay. Hence when pairs is called there is a delay preventing the next pairs call.
