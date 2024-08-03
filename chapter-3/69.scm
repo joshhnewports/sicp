08785fd95f0cb5c6b02ea6b452e083ea12e144b1
joshhnewports <joshhnewports@gmail.com>
2024-08-03 08:49:44 -0700

    Remove redundancy

--- chapter-3/69.scm
+++ chapter-3/69.scm
@@ -3,11 +3,10 @@
 (define (triples s t u)
   (cons-stream (list (stream-car s) (stream-car t) (stream-car u))
 	       (interleave (stream-map (lambda (pair) (cons (stream-car s) pair))
-				       (pairs t u))
+				       (stream-cdr (pairs t u)))
 			   (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
 
 (define pythagorean-triples
   (stream-filter (lambda (triple)
 		   (= (+ (square (car triple)) (square (cadr triple))) (square (caddr triple))))
 		 (triples integers integers integers)))
-		 
