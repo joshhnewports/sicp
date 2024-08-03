7e9a538d9384b9ae558aabf8b0bd422bdc277e8d
joshhnewports <joshhnewports@gmail.com>
2024-08-03 08:13:06 -0700

    Add 69.scm

---
+++ chapter-3/69.scm
@@ -0,0 +1,13 @@
+;;very naive, barely generates any
+
+(define (triples s t u)
+  (cons-stream (list (stream-car s) (stream-car t) (stream-car u))
+	       (interleave (stream-map (lambda (pair) (cons (stream-car s) pair))
+				       (pairs t u))
+			   (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
+
+(define pythagorean-triples
+  (stream-filter (lambda (triple)
+		   (= (+ (square (car triple)) (square (cadr triple))) (square (caddr triple))))
+		 (triples integers integers integers)))
+		 
