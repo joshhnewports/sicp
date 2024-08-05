45633c153e1100636de25d65d2338d230dfd68f4
joshhnewports <joshhnewports@gmail.com>
2024-08-05 13:54:49 -0700

    Fix 70.scm

--- chapter-3/70.scm
+++ chapter-3/70.scm
@@ -19,7 +19,8 @@
 (define (weighted-pairs s t weight)
   (cons-stream (list (stream-car s) (stream-car t))
 	       (merge-weighted (stream-map (lambda (x) (list (stream-car s) x)) (stream-cdr t))
-			       (weighted-pairs (stream-cdr s) (stream-cdr t) weight))))
+			       (weighted-pairs (stream-cdr s) (stream-cdr t) weight)
+			       weight)))
 
 (define a (weighted-pairs integers integers (lambda (pair) (apply + pair))))
 
