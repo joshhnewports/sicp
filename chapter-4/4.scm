32f294b36d1b1de31b85272c0b5ed537ae15479c
joshhnewports <joshhnewports@gmail.com>
2024-08-13 22:00:55 -0700

    Finish 4.scm

--- chapter-4/4.scm
+++ chapter-4/4.scm
@@ -41,3 +41,43 @@
 
 (define (last-predicate? preds)
   (null? (cdr preds)))
+
+;;here we have eval-and and eval-or as derived expressions
+
+(define (and->if exp)
+  (expand-and (and-predicates exp)))
+
+(define (and-predicates exp)
+  (cdr exp))
+
+;;after looking at the definition for cond, we see that cond doesn't have eval anywhere in it.
+;;instead, it returns values that are assumed to be handed off back to eval. we do the same here.
+
+(define (expand-and predicates)
+  (cond ((no-predicates? predicates) true)
+	((last-predicate? predicates)
+	 (first-predicate predicates))
+	(else (make-if (first-predicate predicates)
+		       (expand-and (rest-predicates predicates))
+		       false))))
+
+(define (or->if exp)
+  (expand-or (or-predicates exp)))
+
+(define (or-predicates exp)
+  (cdr exp))
+
+;;returns an exp that is meant to pass the application? predicate in eval.
+;;the car is a lambda which is to be applied to the operand (first-predicate predicates) under eval.
+;;this is essentially a let in our interpreted language. we do not use the underlying scheme's let because
+;;it would seem that derived expressions should be syntactic transformations and to not do any evaluation.
+;;this is subjective but we try to emulate what the book has done.
+;;we do this maneuver to avoid double evaluation.
+(define (expand-or predicates)
+  (if (no-predicates? predicates)
+      false
+      ((make-lambda (p)
+		    (make-if p
+			     p
+			     (expand-or (rest-predicates predicates))))
+       (first-predicate predicates))))
