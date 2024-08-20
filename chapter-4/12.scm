4c7518e021cc8b284c7bb1a8f2c9a47588bbad4c
joshhnewports <joshhnewports@gmail.com>
2024-08-20 02:59:30 -0700

    Finish 12.scm

--- chapter-4/12.scm
+++ chapter-4/12.scm
@@ -1,16 +1,4 @@
-(define (lookup-variable-value var env)
-  (define (env-loop env)
-    (define (scan vars vals)
-      (cond ((null? vars)
-	     (env-loop (enclosing-environment env)))
-	    ((eq? var (car vars)) (car vals))
-	    (else (scan (cdr vars) (cdr vals)))))
-    (if (eq? env the-empty-environment)
-	(error "Unbound variable" var)
-	(let ((frame (first-frame env)))
-	  (scan (frame-variables frame)
-		(frame-values frame)))))
-  (env-loop env))
+;;general procedures
 
 (define (lookup-in-frame var frame)
   (define (scan vars vals)
@@ -20,35 +8,30 @@
   (scan (frame-variables frame) (frame-values frame)))
 
 (define (set-in-frame! var val frame)
-  (lambda (null-exp)
+  (lambda (null-exp)                             ;"inject" an expression into scan
     (define (scan vars vals)
-      (cond ((null? vars) (null-exp))
+      (cond ((null? vars) (null-exp))            ;expect null-exp to be a lambda
 	    ((eq? var (car vars)) (set-car! vals val))
 	    (else (scan (cdr vars) (cdr vals)))))
     (scan (frame-variables frame) (frame-values frame))))
 
+;;implementation for envs
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (lookup-variable-value var env)
+  (if (eq? env the-empty-environment)
+      (error "Unbound variable" var)
+      (let ((val lookup-in-frame var (first-frame env)))
+	(if val
+	    val
+	    (lookup-variable-value var (enclosing-environment env))))))
 
 (define (set-variable-value! var val env)
-  (define (env-loop env)
-    (define (scan vars vals)
-      (cond ((null? vars)
-	     (env-loop (enclosing-environment env)))
-	    ((eq? var (car vars)) (set-car! vals val))
-	    (else (scan (cdr vars) (cdr vals)))))
-    (if (eq? env the-empty-environment)
-	(error "Unbound variable -- SET!" var)
-	(let ((frame (first-frame env)))
-	  (scan (frame-variables frame)
-		(frame-values frame)))))
-  (env-loop env))
+  (if (eq? env the-empty-environment)
+      (error "Unbound variable -- SET!" var)
+      ((set-in-frame! var val frame)
+       (lambda () (set-variable-value! var val (enclosing-environment env))))))
 
 (define (define-variable! var val env)
   (let ((frame (first-frame env)))
-    (define (scan vars vals)
-      (cond ((null? vars)
-	     (add-binding-to-frame! var val frame))
-	    ((eq? var (car vars)) (set-car! vals val))
-	    (else (scan (cdr vars) (cdr vals)))))
-    (scan (frame-variables frame) (frame-values frame))))
+    ((set-in-frame! var val frame)
+     (lambda () (add-binding-to-frame! var val frame))))) ;we dont traverse through enclosing environments here
