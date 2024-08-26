21ea55298ef33a13c80d69fb6dcae4e9fa07b00a
joshhnewports <joshhnewports@gmail.com>
2024-08-26 05:43:41 -0700

    Add 22.scm

---
+++ chapter-4/22.scm
@@ -0,0 +1,13 @@
+(define (analyze exp)
+  (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
+	((quoted? exp) (analyze-quoted exp))
+	((variable? exp) (analyze-variable exp))
+	((assignment? exp) (analyze-assignment exp))
+	((definition? exp) (analyze-definition exp))
+	((if? exp) (analyze-if exp))
+	((lambda? exp) (analyze-lambda exp))
+	((let? exp) (analyze (let->combination exp)))     ;here!
+	((begin? exp) (analyze-sequence (begin-actions exp)))
+	((cond? exp) (analyze (cond->if exp)))
+	((application? exp) (analyze-application exp))
+	(else (error "Unknown expression type: ANALYZE" exp))))
