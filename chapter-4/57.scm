0abc43b9763602362f774c0935a644b63fc6fec4
joshhnewports <joshhnewports@gmail.com>
2024-09-13 12:44:04 -0700

    Finish 57.scm

--- chapter-4/57.scm
+++ chapter-4/57.scm
@@ -1,10 +1,14 @@
+;;originally misinterpreted the rule while reading
 (rule (can-replace ?person-1 ?person-2)
-      (job ?person-1 (?division . ?job-1))
-      (job ?person-2 (?division . ?job-2))
-      (and (or (same ?job-1 ?job-2)
-	       (and (job ?person-3 (?division . ?job-1))
-		    (can-do-job ?job-1 ?job-2)))
-	   (not (same person-1 person-2))))
+      (and (job ?person-1 ?job-1)
+	   (job ?person-2 ?job-2)
+	   (or (same ?job-1 ?job-2)
+	       (can-do-job ?job-1 ?job-2))
+	   (not (same ?person-1 ?person-2))))
 
 (can-replace ?x (Fect Cy D))
 
+(and (salary ?person-1 ?amount-1)
+     (salary ?person-2 ?amount-2)
+     (lisp-value < ?amount-1 ?amount-2)
+     (can-replace ?person-1 ?person-2))
