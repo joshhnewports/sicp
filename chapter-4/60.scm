518fcb0b296bc3eb28b57b3a2c5340256e146444
joshhnewports <joshhnewports@gmail.com>
2024-09-15 13:59:22 -0700

    Add 60.scm

---
+++ chapter-4/60.scm
@@ -0,0 +1,3 @@
+When ?person-1 is Alyssa then ?person-2 is Cy and vice versa. Find a way to compare two people such that if a person
+as ?person-1 compared to another person ?person-2 does not satisfy if their values were switched. We could do this
+by lisp-value.
