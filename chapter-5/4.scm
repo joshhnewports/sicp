8dc5ac3a4f20b0798571ed12aabf852d28ee7dac
joshhnewports <joshhnewports@gmail.com>
2024-09-24 13:39:17 -0700

    Continue 4.scm

--- chapter-5/4.scm
+++ chapter-5/4.scm
@@ -1,3 +1,26 @@
+;;recursive
+(controller
+ (assign continue (label done))        ;where to return after computing expt. the machine is only expt, so its done
+
+ expt                                  ;expand out expt process by saving a (label reduce-expt) to stack
+ (test (op =) (reg n) (const 0))
+ (branch (label base-case))
+ (save continue)                       ;only continue needs to be saved, not n
+ (assign continue (label reduce-expt)) ;by reaching this instruction that means that the process expanded
+ (assign n (op -) (reg n) (const 1))
+ (goto (label expt))
+
+ reduce-expt                           ;reduce process by restoring a reduce-expt in stack
+ (assign val (op *) (reg val) (reg b)) ;after base-case assigns val to 1, we multiply val by b
+ (restore continue)                   
+ (goto (reg continue))                 ;and if val is b^n, then continue should hold (label done), and we are done
+
+ base-case
+ (assign val (const 1))
+ (goto (reg continue))                 ;either goes to done or begins to reduce the process
+ 
+ done)
+
 ;;iterative
 (controller
  (assign counter (reg n))
