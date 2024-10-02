f35fd004f5d2035bcab1127cf4a360121fb799b9
joshhnewports <joshhnewports@gmail.com>
2024-10-02 14:52:08 -0700

    Finish 11.scm

--- chapter-5/11.scm
+++ chapter-5/11.scm
@@ -19,18 +19,24 @@
 		(else (error "Restored value not from saved register -- RESTORE" inst reg-name))))))))
 
 ;;c
+;;make-new-machine has a stack-table analogous to register-table.
+;;initialize-operation should initialize the register stacks, as the exercised demands, and it is up to the machine
+;;designer using make-machine to call this operation.
+;;it is up to make-save and make-restore to fetch the correct stack that is associated with the register that is
+;;saved or restored.
+;;we shouldnt change make-stack or make-register.
 
 (define (make-new-machine)
   (let ((pc (make-register 'pc))
         (flag (make-register 'flag))
         (the-instruction-sequence '())
-	(stack-table '()))
+	(stack-table '()))                                            ;car is reg-name, cdr is the associated stack
     (let ((register-table (list (list 'pc pc) (list 'flag flag))))
       (let ((the-ops 
-	     (list (list 'initialize-stack                            ;;user must call this operation in their insts
+	     (list (list 'initialize-stack                            ;user must call this operation in their insts
 			 (lambda ()
 			   (for-each (lambda (register)
-				       (set! stack-table              ;;(car register) is the name of the register
+				       (set! stack-table              ;(car register) is the name of the register
 					     (cons (list (car register) ((make-stack) 'initialize)) 
 						   stack-table)))
 				     register-table))))))
@@ -63,20 +69,28 @@
 		((eq? message 'get-register) lookup-register)
 		((eq? message 'install-operations)
 		 (lambda (ops) (set! the-ops (append the-ops ops))))
-		((eq? message 'stack) stack-table)
+		((eq? message 'stack) stack-table)                       ;return stack-table
 		((eq? message 'operations) the-ops)
 		(else (error "Unknown request -- MACHINE" message))))
 	dispatch))))
 
 (define (make-save inst machine stack pc)
-  (let ((reg (get-register machine
-                           (stack-inst-reg-name inst))))
-    (lambda ()
-      (push stack (get-contents reg))
-      (advance-pc pc))))
+  (let ((reg-name (stack-inst-reg-name inst)))
+    (let ((reg (get-register machine reg-name))
+	  (reg-stack (assoc reg-name stack)))    ;get the stack pair in the table associated with the register
+      (lambda ()
+	(if reg-stack
+	    (begin (push (cdr reg-stack) (get-contents reg))  ;cdr reg-stack is the stack
+		   (advance-pc pc))
+	    (error "Stack not initialized -- SAVE" reg-name))))))
+
 (define (make-restore inst machine stack pc)
-  (let ((reg (get-register machine
-                           (stack-inst-reg-name inst))))
-    (lambda ()
-      (set-contents! reg (pop stack))    
-      (advance-pc pc))))
+  (let ((reg-name (stack-inst-reg-name inst)))
+    (let ((reg (get-register machine reg-name))
+	  (reg-stack (assoc reg-name stack)))
+      (lambda ()
+	(if reg-stack
+	    (begin (set-contents! (cdr reg-stack) (pop stack))
+		   (advance-pc pc))
+	    (error "Stack not initialized -- RESTORE" reg-name))))))
+      
