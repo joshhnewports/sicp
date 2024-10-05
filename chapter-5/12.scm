aeed138aecfea12c7d748bec1b957d0ad82767fb
joshhnewports <joshhnewports@gmail.com>
2024-10-04 22:11:35 -0700

    Continue 12.scm

--- chapter-5/12.scm
+++ chapter-5/12.scm
@@ -1,9 +1,12 @@
+;;the information for the data paths should not require that the machine be run with start but should be
+;;generated only once the machine is defined. that is, after the instructions have been assembled.
+
 (define (make-new-machine)
   (let ((pc (make-register 'pc))
         (flag (make-register 'flag))
         (stack (make-stack))
         (the-instruction-sequence '())
-	(instructions '((assign) (test) (branch) (goto) (save) (restore) (perform))
+	(instructions '((*assign*) (*test*) (*branch*) (*goto*) (*save*) (*restore*) (*perform*)))
 	(goto-registers '())
 	(save-restore-registers '())
 	(register-sources '()))
@@ -53,16 +56,57 @@
 (define (make-assign inst machine labels operations pc)
   (let ((target
          (get-register machine (assign-reg-name inst)))
-        (value-exp (assign-value-exp inst))
-	(instructions (machine 'instructions)))
-    (let ((assign-entries (assoc 'assign instructions))
+        (value-exp (assign-value-exp inst)))
+    (let ((assign-entries (assoc '*assign* (machine 'instructions)))
 	  (value-proc
            (if (operation-exp? value-exp)
                (make-operation-exp
                 value-exp machine labels operations)
                (make-primitive-exp
                 (car value-exp) machine labels))))
-      (set-cdr! assign-entries (cons inst (cdr assign-entries)))
-      (lambda ()                ; execution procedure for assign
+      (set-cdr! assign-entries (cons inst (cdr assign-entries))) ;add inst to instructions
+      (lambda ()
         (set-contents! target (value-proc))
         (advance-pc pc)))))
+
+(define (make-test inst machine labels operations flag pc)
+  (let ((condition (test-condition inst)))
+    (if (operation-exp? condition)
+        (let ((test-entries (assoc '*test* (machine 'instructions)))
+	      (condition-proc
+	       (make-operation-exp
+                condition machine labels operations)))
+	  (set-cdr! test-entries (cons inst (cdr test-entries))) ;here!
+	  (lambda ()
+	    (set-contents! flag (condition-proc))
+	    (advance-pc pc)))
+        (error "Bad TEST instruction -- ASSEMBLE" inst))))
+
+(define (make-branch inst machine labels flag pc)
+  (let ((dest (branch-dest inst)))
+    (if (label-exp? dest)
+        (let ((insts
+               (lookup-label labels (label-exp-label dest)))
+	      (branch-entries (assoc '*branch* (machine 'instructions))))
+	  (set-cdr! branch-entries (cons inst (cdr branch-entries)))
+          (lambda ()
+            (if (get-contents flag)
+                (set-contents! pc insts)
+                (advance-pc pc))))
+        (error "Bad BRANCH instruction -- ASSEMBLE" inst))))
+
+(define (make-goto inst machine labels pc)
+  (let ((dest (goto-dest inst)))
+    (cond ((label-exp? dest)
+           (let ((insts
+                  (lookup-label labels
+                                (label-exp-label dest))))
+             (lambda () (set-contents! pc insts))))
+          ((register-exp? dest)
+           (let ((reg
+                  (get-register machine
+                                (register-exp-reg dest))))
+             (lambda ()
+               (set-contents! pc (get-contents reg)))))
+          (else (error "Bad GOTO instruction -- ASSEMBLE"
+                       inst)))))
