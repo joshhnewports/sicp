321eb74ff60802c94b5b6a72d4a7bf8f5d341a0e
joshhnewports <joshhnewports@gmail.com>
2024-12-04 18:08:46 -0800

    Continue 21.scm

--- chapter-5/21.scm
+++ chapter-5/21.scm
@@ -6,13 +6,36 @@ cl-loop
 (test (op null?) (reg tree))
 (branch (label null-case))
 (test (op pair?) (reg tree))
-;;recursive case
-(save continue)
-(assign continue (label after-car))
-(goto cl-loop)
+(branch (label recursive-case))
+;;(not (pair? tree))
+(assign val (const 1))
+(goto (reg continue))
 
 after-car
+(restore tree)
 (restore continue)
 (save continue)
+;;setup computation of (count-leaves (cdr tree))
 (save val)
 (assign continue (label after-cdr))
+(assign tree (op cdr) (reg tree))
+(goto (label cl-loop))
+
+after-cdr ;val has the value (count-leaves (car tree))
+(assign tree (reg val))
+(restore val)
+(assign val (op +) (reg tree) (reg val))
+(restore continue)
+(goto (reg continue))
+
+recursive-case
+(save continue)
+(assign continue (label after-car))
+(save tree)
+(assign tree (op car) (reg tree))
+(goto cl-loop)
+
+null-case
+(assign val (const 0))
+(goto (reg continue))
+
