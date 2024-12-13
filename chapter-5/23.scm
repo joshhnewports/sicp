5e3a9af9590f3a9f0e04cd7557414b692a96e0d4
joshhnewports <joshhnewports@gmail.com>
2024-12-12 23:56:13 -0800

    Continue 23.scm

--- chapter-5/23.scm
+++ chapter-5/23.scm
@@ -1,7 +1,23 @@
+;;syntactically transform to an application set in the exp register, then goto eval-dispatch.
+;;cons a lambda expression to the list of values
 ev-let
 (assign unev (op let-bindings) (reg exp))
 (assign exp (op let-exp) (reg exp))
+(assign argl (const ()))
 
+ev-separate-let-bindings ;get vals in argl
+(test (op null?) (reg unev))
+(branch (label after-separate))
+(save continue)
+(save unev)
+(assign continue (label ev-collect-let-bindings))
+
+
+after-separate
+(goto (reg continue))
+ 
+(assign exp (op cons) (reg unev) (reg argl))
+(goto (label eval-dispatch))
 
 (let ((var val))
   exp)
