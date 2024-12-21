64b61072094b87e4b3f6a9b8dca772d88862e446
joshhnewports <joshhnewports@gmail.com>
2024-12-20 23:55:30 -0800

    Finish 24.scm

--- chapter-5/24.scm
+++ chapter-5/24.scm
@@ -4,23 +4,26 @@ ev-cond
 ev-cond-loop
 (test (op no-clauses?) (reg exp))
 (branch (label ev-cond-null-case))
-(save exp)
+(save exp) ;clauses
+(assign exp (op first-clause) (reg exp))
+(test (op else-clause?) (reg exp))
+(branch (label ev-cond-else-case))
+(save exp) ;first clause
+(assign exp (op cond-predicate) (reg exp))
 (save env)
 (save continue)
-(assign exp (op first-clause) (reg exp))
-(assign exp (op cond-predicate) (reg exp))
-(test (op else-predicate?) (reg exp))
-(branch (label ev-cond-else-case))
 (assign continue (label ev-cond-decide))
 (goto (label eval-dispatch))
 
 ev-cond-decide
 (restore continue)
 (restore env)
-(restore exp)
+(restore exp) ;first clause
+(assign unev (reg exp)) ;preserve clause in case the predicate is true
+(restore exp) ;clauses
 (test (op true?) (reg val))
 (branch (label ev-cond-consequent))
-(assign exp (op rest-predicates) (reg exp))
+(assign exp (op rest-clauses) (reg exp))
 (goto (label ev-cond-loop))
 
 ev-cond-consequent
@@ -33,8 +36,9 @@ ev-cond-null-case
 (goto (label eval-dispatch))
 
 ev-cond-else-case
-(restore continue)
-(restore env)
+;;clear the clauses from the stack but keep exp the same
+(assign unev (reg exp))
 (restore exp)
+(assign exp (reg unev))
 (goto (label ev-cond-consequent))
 
