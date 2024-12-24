4d67676ff542aa13022f1a251de0b78756a3d9b7
joshhnewports <joshhnewports@gmail.com>
2024-12-23 23:59:42 -0800

    Add 28.scm

---
+++ chapter-5/28.scm
@@ -0,0 +1,11 @@
+;;iterative
+1 70  17
+2 107 20
+3 144 23
+
+;;recursive
+1 18 11
+2 52 19
+3 86 27
+
+The space grows linearly with the input, even for iterative processes, under the non-tail-recursive evaluator. Max space = 3n + 14.
