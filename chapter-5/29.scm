a276b451fcbd19528badac7ee0450b2ccf24ec12
joshhnewports <joshhnewports@gmail.com>
2024-12-24 16:01:42 -0800

    Start 29.scm

---
+++ chapter-5/29.scm
@@ -0,0 +1,14 @@
+2 72   13 1
+3 128  18 2
+4 240  23 3
+5 408  28 5
+6 688  33 8
+7 1136 38 13
+
+Max depth = 5n + 3.
+
+Let S(n) be the number of pushes in computing Fib(n). Then S(n) = S(n - 1) + S(n - 2) + 40.
+
+S(4) = a * 5 + b, a = 48
+S(5) = a * 8 + b = 408, a = 51
+S(6) = a * 13 + b = 688,
