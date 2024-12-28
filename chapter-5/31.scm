c3feb0509c7191686f911be3500de7b58cf50cb0
joshhnewports <joshhnewports@gmail.com>
2024-12-27 23:18:28 -0800

    Continue 31.scm

--- chapter-5/31.scm
+++ chapter-5/31.scm
@@ -1,8 +1,16 @@
 (save env)
+f
 (restore env)
+
 (save proc)
 (save env)
 (save argl)
+'x
 (restore argl)
 (restore env)
+(save argl)
+'y
+(restore argl)
 (restore proc)
+
+Evaluating f does not recurse into an evaluation causing env to change. The same for the evaluation of 'x and 'y not causing proc, env, and argl to change. All operations are superfluous.
