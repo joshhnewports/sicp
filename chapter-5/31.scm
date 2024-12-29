72af19c4c514f5316e890256ed14c5832ea77aa7
joshhnewports <joshhnewports@gmail.com>
2024-12-28 23:59:42 -0800

    Continue 31.scm

--- chapter-5/31.scm
+++ chapter-5/31.scm
@@ -1,16 +1,5 @@
-(save env)
-f
-(restore env)
-
-(save proc)
-(save env)
-(save argl)
-'x
-(restore argl)
-(restore env)
-(save argl)
-'y
-(restore argl)
-(restore proc)
-
+(f 'x 'y)
 Evaluating f does not recurse into an evaluation causing env to change. The same for the evaluation of 'x and 'y not causing proc, env, and argl to change. All operations are superfluous.
+
+((f) 'x 'y)
+The operator is applied to no arguments.
