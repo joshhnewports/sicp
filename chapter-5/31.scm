6dc0899d197866fb7afbce88f5b95200046a0c0c
joshhnewports <joshhnewports@gmail.com>
2024-12-29 23:56:55 -0800

    Good enough 31.scm

--- chapter-5/31.scm
+++ chapter-5/31.scm
@@ -2,4 +2,10 @@
 Evaluating f does not recurse into an evaluation causing env to change. The same for the evaluation of 'x and 'y not causing proc, env, and argl to change. All operations are superfluous.
 
 ((f) 'x 'y)
-The operator is applied to no arguments.
+argl env are necessary
+
+(f (g 'x) y)
+proc argl env are necessary
+
+(f (g 'x) 'y)
+proc argl env are necessary
